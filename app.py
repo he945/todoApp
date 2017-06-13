e8f783ba477fde699b9e340bd7bc5d28cd4db28a
haro25 <jumpshot99@gmail.com>
2017-06-12 22:16:45 -0400

    initial commit

--- app.py
+++ app.py
@@ -89,4 +89,5 @@ def getTasks():
     return jsonify({"tasks":serialized_tasks, "notDoneCount": notDoneCount})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
